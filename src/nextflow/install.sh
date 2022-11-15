74552ef4d4a835c4bf69d8aacf68036c9a6c29d1
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-15 23:53:33 +0000

    tweak: use sdkman to install java

--- src/nextflow/install.sh
+++ src/nextflow/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+curl -s "https://get.sdkman.io/" | bash sdk init sdkman install java
 curl -s https://get.nextflow.io | bash
 sudo chmod +rx nextflow
 sudo mv nextflow /usr/local/bin/
