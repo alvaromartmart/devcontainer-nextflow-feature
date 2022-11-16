bffce604d7d62b69a77517f1f6f87c213e42e2b9
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-16 00:25:20 +0000

    fix: sdk install java attempt 2

--- src/nextflow/install.sh
+++ src/nextflow/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl -s "https://get.sdkman.io/" | bash 
 source "/root/.sdkman/bin/sdkman-init.sh"
-sdk install java 17
+sdk install java 17.0.5-tem
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 curl -s https://get.nextflow.io | bash
 sudo chmod +rx nextflow
