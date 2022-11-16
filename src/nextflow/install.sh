14388e9069f82eb2dbe8f9417e40640fd6606e8d
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-16 00:16:22 +0000

    fix: set JAVA_HOME

--- src/nextflow/install.sh
+++ src/nextflow/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 curl -s "https://get.sdkman.io/" | bash 
 source "/root/.sdkman/bin/sdkman-init.sh"
+export JAVA_HOME=$(/usr/libexec/java_home -v 11)
 curl -s https://get.nextflow.io | bash
 sudo chmod +rx nextflow
 sudo mv nextflow /usr/local/bin/
