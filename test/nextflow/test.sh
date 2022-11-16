a12f87a04e1788e0f7008b8cbc6d59948bd6cfed
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-16 00:00:26 +0000

    test: more test fixes

--- test/nextflow/test.sh
+++ test/nextflow/test.sh
@@ -23,7 +23,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" nextflow | grep 'nextflowd'
+check "version" nextflow | grep 'nextflow'
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
