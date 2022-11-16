ee6c9de6b6a034b32e909b9b5b11f7621f6db197
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-16 00:14:08 +0000

    tweak: change sdkman setup attempt 3

--- src/nextflow/install.sh
+++ src/nextflow/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -s "https://get.sdkman.io/" | bash 
-source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+source "/root/.sdkman/bin/sdkman-init.sh"
 curl -s https://get.nextflow.io | bash
 sudo chmod +rx nextflow
 sudo mv nextflow /usr/local/bin/
