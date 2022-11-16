f023e00f8cb988e0d47202b8664133d26f7aa7d4
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2022-11-16 07:39:01 +0000

    tweak: sdkman setup

--- src/nextflow/install.sh
+++ src/nextflow/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+# Setup Java
 curl -s "https://get.sdkman.io/" | bash 
-source "/root/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 17.0.5-tem
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+
+# Install nextflow
 curl -s https://get.nextflow.io | bash
-sudo chmod +rx nextflow
+sudo chmod a+rx nextflow
 sudo mv nextflow /usr/local/bin/
